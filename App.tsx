67e75839c59ffc2009699c73555e806eee88d6a4
thoriqdharmawan <thoriqd21@gmail.com>
2020-11-29 21:44:24 +0700

    feat: carousel

--- App.tsx
+++ App.tsx
@@ -1,10 +1,191 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StatusBar,
+  Animated,
+  Text,
+  Image,
+  View,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
+
+const bgs = ["#A5BBFF", "#DDBEFE", "#FF63ED", "#B98EFF"];
+const DATA = [
+  {
+    key: "3571572",
+    title: "Multi-lateral intermediate moratorium",
+    description:
+      "I'll back up the multi-byte XSS matrix, that should feed the SCSI application!",
+    image: "https://image.flaticon.com/icons/png/256/3571/3571572.png",
+  },
+  {
+    key: "3571747",
+    title: "Automated radical data-warehouse",
+    description:
+      "Use the optical SAS system, then you can navigate the auxiliary alarm!",
+    image: "https://image.flaticon.com/icons/png/256/3571/3571747.png",
+  },
+  {
+    key: "3571680",
+    title: "Inverse attitude-oriented system engine",
+    description:
+      "The ADP array is down, compress the online sensor so we can input the HTTP panel!",
+    image: "https://image.flaticon.com/icons/png/256/3571/3571680.png",
+  },
+  {
+    key: "3571603",
+    title: "Monitored global data-warehouse",
+    description: "We need to program the open-source IB interface!",
+    image: "https://image.flaticon.com/icons/png/256/3571/3571603.png",
+  },
+];
+
+const Indicator = ({ scrollX }) => {
+  return (
+    <View style={{ position: "absolute", bottom: 100, flexDirection: "row" }}>
+      {DATA.map((_, i) => {
+        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.8, 1.4, 0.8],
+          extrapolate: "clamp",
+        });
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.6, 0.9, 0.6],
+          extrapolate: "clamp",
+        });
+        return (
+          <Animated.View
+            key={`indicator-${i}`}
+            style={{
+              height: 10,
+              width: 10,
+              borderRadius: 5,
+              backgroundColor: "#fff",
+              margin: 10,
+              opacity,
+              transform: [
+                {
+                  scale,
+                },
+              ],
+            }}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+const Backdrop = ({ scrollX }) => {
+  const backgroundColor = scrollX.interpolate({
+    inputRange: bgs.map((_, i) => i * width),
+    outputRange: bgs.map((bg) => bg),
+  });
+  return (
+    <Animated.View
+      style={[StyleSheet.absoluteFillObject, { backgroundColor }]}
+    />
+  );
+};
+
+const Square = ({ scrollX }) => {
+  const YOLO = Animated.modulo(
+    Animated.divide(Animated.modulo(scrollX, width), new Animated.Value(width)),
+    1
+  );
+
+  const rotate = YOLO.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: ["35deg", "0deg", "35deg"],
+  });
+  const translateX = YOLO.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0, -height, 0],
+  });
+  return (
+    <Animated.View
+      style={{
+        width: height,
+        height: height,
+        backgroundColor: "#fff",
+        borderRadius: 86,
+        position: "absolute",
+        top: -height * 0.6,
+        left: -height * 0.3,
+        transform: [
+          {
+            rotate,
+          },
+          {
+            translateX
+          }
+        ],
+      }}
+    />
+  );
+};
 
 export default function App() {
+  const scrollx = React.useRef(new Animated.Value(0)).current;
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar hidden />
+      <Backdrop scrollX={scrollx} />
+      <Square scrollX={scrollx} />
+      <Animated.FlatList
+        data={DATA}
+        horizontal
+        scrollEventThrottle={32}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.key}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollx } } }],
+          { useNativeDriver: false }
+        )}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, alignItems: "center", padding: 20 }}>
+              <View
+                style={{
+                  flex: 0.7,
+                  // backgroundColor: "red",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={{ uri: item.image }}
+                  style={{
+                    width: width / 2,
+                    height: height / 2,
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+              <View style={{ flex: 0.3 }}>
+                <Text
+                  style={{
+                    color: "#fff",
+                    fontWeight: "800",
+                    fontSize: 28,
+                    marginBottom: 10,
+                    marginTop: 32
+                  }}
+                >
+                  {item.title}
+                </Text>
+                <Text style={{ color: "#fff", fontWeight: "300" }}>
+                  {item.description}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+      <Indicator scrollX={scrollx} />
     </View>
   );
 }
@@ -12,8 +193,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
